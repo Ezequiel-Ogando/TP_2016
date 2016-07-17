fd1d929b1d2272c69ee208feeeee97614d98071d
Ezequiel.Ogando <oguisan_90@hotmail.com>
2016-07-17 20:40:06 -0300

    version 1.1

--- MVNP/src/main/java/com/mycompany/mvnp/camara.java
+++ MVNP/src/main/java/com/mycompany/mvnp/camara.java
@@ -12,15 +12,10 @@ import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyImages
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectedFaces;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ImageFace;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassification;
-import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassifier;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualRecognitionOptions;
 import java.io.File;
-import org.json.*;
 
 import static java.lang.Thread.sleep;
-import java.util.ArrayList;
-import java.util.List;
-
 
 
 /**
@@ -32,12 +27,10 @@ public class camara {
     /**
      * @param args the command line arguments
      */
-    private static Clasificador clasificador = new Clasificador();
-    
     public static void main(String[] args) {
         // TODO code application logic here
       VisualRecognition service = new VisualRecognition(VisualRecognition.VERSION_DATE_2016_05_19);
-      service.setApiKey("ebe3359ae70494ba901dfde15b81dcf6816ce9e3");
+      service.setApiKey("9627434df5f2fbab7ccbae527d934b2a7d0b36bd");
       
       String ruta;
       if ( args.length > 0 )
@@ -54,51 +47,16 @@ public class camara {
       System.out.println("Faces detection");
       
       //VisualRecognitionOptions options = new VisualRecognitionOptions.Builder().images(new File("/home/utnso/31.jpg")).build();
-      VisualRecognitionOptions options = new VisualRecognitionOptions.Builder().images(new File(ruta)).build();
+      VisualRecognitionOptions options = new VisualRecognitionOptions.Builder().images(new File(ruta.toString())).build();
       
       DetectedFaces result = service.detectFaces(options).execute();
       //result.getImages().get(0);
-      //System.out.println(result.getImages().toArray().length);
+      System.out.println(result.getImages().toArray().length);
       
       String cad = result.toString();
       System.out.println(result);
-      
-       char mayoria = clasificador.mayoria(result.toString());
-//       System.out.println("genero predominante: " + mayoria);
-       
-       Edad rango = clasificador.rango_edad(mayoria, cad);
-       System.out.println("rango: " + rango.get_inferior() + "-" + rango.get_superior());
-       
-      
-      //ArrayList <VisualClassifier> lista = new ArrayList<VisualClassifier>();
-       List<VisualClassifier> lista =  service.getClassifiers().execute();
-       System.out.println("cantidad de clasificadores: " + lista.size() );
-       System.out.println(lista.get(0).toString());
-       System.out.println(lista.get(1).toString());
-       
-       //------------------- ARMAR LISTA DE CLASIFICADORES
-       List<String> lista_clasif = new ArrayList<String>();
-       //JSONObject obj_clasif = new JSONObject(lista.toString());
-       
-       for (int i=0; i < lista.size(); i++)
-       {
-           JSONObject obj_clasif = new JSONObject(lista.get(i).toString());
-           lista_clasif.add(obj_clasif.getString("classifier_id"));
-       }
-       lista_clasif.add("default");
-       
-       ClassifyImagesOptions options_cl = new ClassifyImagesOptions.Builder().images(new File(ruta)).classifierIds(lista_clasif).build();
-       VisualClassification clasificaciones = service.classify(options_cl).execute();
-       System.out.println("Resultado de clasificacion: ");
-       System.out.println(clasificaciones.toString());
               
       
-    }
-    
-    public void calsificar()
-    {
-        
-        
     }
     
 }
